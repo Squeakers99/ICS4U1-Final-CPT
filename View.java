0ca7773fcdd5cd44a6fa22651339a0bd41771549
Soheil Rajabali <soheilrajabali@gmail.com>
2024-05-30 09:09:33 -0400

    Button Press Chat Implementation

--- View.java
+++ View.java
@@ -5,9 +5,6 @@ import javax.swing.*;
 
 public class View implements ActionListener {
 
-    String strTempArray1[];
-    String strTempArray2[][];
-
     Model theModel = new Model(this);
 
     MainScreen theMainScreen = new MainScreen();
@@ -28,9 +25,9 @@ public class View implements ActionListener {
                 theMainScreen.theNameField.setText("Player");
             }
         } else if (e.getSource() == theMainScreen.theJoinButton) {
-            if(theMainScreen.theNameField.getText().equals("")){
+            if (theMainScreen.theNameField.getText().equals("")) {
                 theMainScreen.theNameField.setText("Player");
-            }else{
+            } else {
                 theModel.strUsername = theMainScreen.theNameField.getText();
                 theFrame.setContentPane(theIPScreen);
             }
@@ -39,19 +36,25 @@ public class View implements ActionListener {
         } else if (e.getSource() == theServerLobby.theChatField) {
             if (theModel.blnIsHost) {
                 theServerLobby.theChatArea.append(theMainScreen.theNameField.getText() + ": " + theServerLobby.theChatField.getText() + "\n");
-                theModel.sendMessage(theModel.strUsername, "1", "0", "1", theServerLobby.theChatField.getText(), null, null);
+                theModel.sendMessage(theModel.strUsername, "1", theModel.strRole, "1", theServerLobby.theChatField.getText(), null, null);
             } else {
-                theModel.sendMessage(theModel.strUsername, "0", "0", "1", theServerLobby.theChatField.getText(), null, null);
+                theModel.sendMessage(theModel.strUsername, "0", theModel.strRole, "1", theServerLobby.theChatField.getText(), null, null);
             }
             theServerLobby.theChatField.setText("");
-        } else if(e.getSource() == theIPScreen.theJoinButton){
-            if(theModel.initializeClient(theMainScreen.theNameField.getText(), theIPScreen.theIPField.getText()) && !theIPScreen.theIPField.getText().equals("") && !theIPScreen.theIPField.getText().equals(theModel.theSocket.getMyAddress())){
+        } else if (e.getSource() == theIPScreen.theJoinButton) {
+            if (theModel.initializeClient(theMainScreen.theNameField.getText(), theIPScreen.theIPField.getText()) && !theIPScreen.theIPField.getText().equals("")) {
                 theServerLobby.theIPAdress.setText(theIPScreen.theIPField.getText());
                 theFrame.setContentPane(theServerLobby);
-            }else{
+            } else {
                 theIPScreen.theErrorMessage.setText("Server Not Found");
             }
-        }else if (e.getSource() == theModel.theSocket) {
+        } else if (e.getSource() == theServerLobby.theRedButton) {
+            SwitchRoles("1");
+        } else if (e.getSource() == theServerLobby.theBlackButton) {
+            SwitchRoles("2");
+        } else if (e.getSource() == theServerLobby.theSpectatorButton) {
+            SwitchRoles("0");
+        } else if (e.getSource() == theModel.theSocket) {
             //Gets the message from the socket
             theModel.receiveMessage(theModel.theSocket.readText());
 
@@ -59,41 +62,115 @@ public class View implements ActionListener {
             if (theModel.strMessage[1].equals("0") && theModel.blnIsHost) {
                 //Action 0: Client Connected
                 if (theModel.strMessage[3].equals("0")) {
+                    //Updates the variables
                     theModel.intPlayersConnected++;
-                    strTempArray1 = theModel.StringToStrArray1(theModel.strMessage[4]);
-                    theModel.strPlayerList[theModel.intPlayersConnected - 1][0] = strTempArray1[0];
-                    theModel.strPlayerList[theModel.intPlayersConnected - 1][1] = strTempArray1[1];
+                    theModel.strPlayerList[theModel.intPlayersConnected - 1] = theModel.strMessage[4];
                     theModel.intRoleData[0]++;
+
+                    //Updates the Panel
                     theServerLobby.theSpectatorPlayers.setText("Spectators: " + theModel.intRoleData[0]);
-                    theServerLobby.theChatArea.append("Server: " + theModel.strPlayerList[theModel.intPlayersConnected - 1][0] + " has joined the lobby\n");
-                    theModel.sendMessage(theModel.strUsername, "1", null, "0", theModel.ArrayToString2(theModel.strPlayerList), String.valueOf(theModel.intPlayersConnected),theModel.ArrayToString1(theModel.intRoleData));
+                    theServerLobby.theChatArea.append("Server: " + theModel.strPlayerList[theModel.intPlayersConnected - 1] + " has joined the lobby\n");
+
+                    //Sends the message to all clients
+                    theModel.sendMessage(theModel.strUsername, "1", theModel.strRole, "0", theModel.ArrayToString1(theModel.strPlayerList), theModel.ArrayToString1(theModel.intRoleData), String.valueOf(theModel.intPlayersConnected));
                 }
                 //Action 1: Server Lobby Text
                 if (theModel.strMessage[3].equals("1")) {
                     theServerLobby.theChatArea.append(theModel.strMessage[0] + ": " + theModel.strMessage[4] + "\n");
                     theModel.sendMessage(theModel.strMessage[0], "1", null, "1", theModel.strMessage[4], null, null);
                 }
-            //Intended for Client
+                //Action 2: Client Role Change
+                if (theModel.strMessage[3].equals("2")) {
+                    UpdateRoles(theModel.strMessage[0], theModel.strMessage[2], theModel.strMessage[4]);
+                }
+                //Intended for Client
             } else if (theModel.strMessage[1].equals("1") && !theModel.blnIsHost) {
                 //Action 0: New Client Joined
                 if (theModel.strMessage[3].equals("0")) {
-                    strTempArray2 = theModel.StringToArray2(theModel.strMessage[4]);
-                    theModel.strPlayerList = strTempArray2;
-                    theModel.intPlayersConnected = theModel.strPlayerList.length;
-                    theServerLobby.theServerTitle.setText(theModel.strPlayerList[0][0] + "'s Lobby");
-                    theModel.intPlayersConnected = Integer.parseInt(theModel.strMessage[5]);
-                    theModel.intRoleData = theModel.StringToIntArray1(theModel.strMessage[6]);
+                    //Updates all variables
+                    theModel.strPlayerList = theModel.StringToStrArray1(theModel.strMessage[4]);
+                    theModel.intRoleData = theModel.StringToIntArray1(theModel.strMessage[5]);
+                    theModel.intPlayersConnected = Integer.parseInt(theModel.strMessage[6]);
+
+                    //Updates the Panel
+                    theServerLobby.theServerTitle.setText(theModel.strPlayerList[0] + "'s Lobby");
                     theServerLobby.theSpectatorPlayers.setText("Spectators: " + theModel.intRoleData[0]);
-                    theServerLobby.theChatArea.append("Server: " + theModel.strPlayerList[theModel.intPlayersConnected - 1][0] + " has joined the lobby\n");
+                    theServerLobby.theRedPlayers.setText("Red Players: " + theModel.intRoleData[1]);
+                    theServerLobby.theBlackPlayers.setText("Black Players: " + theModel.intRoleData[2]);
+                    theServerLobby.theChatArea.append("Server: " + theModel.strPlayerList[theModel.intPlayersConnected - 1] + " has joined the lobby\n");
+                    theServerLobby.theStartButton.setEnabled(false);
+
+                    //Updates the frame
                     theFrame.setContentPane(theServerLobby);
                 }
                 //Action 1: Server Lobby Text
                 if (theModel.strMessage[3].equals("1")) {
                     theServerLobby.theChatArea.append(theModel.strMessage[0] + ": " + theModel.strMessage[4] + "\n");
                 }
+                //Action 2: Client Role Change
+                if (theModel.strMessage[3].equals("2")) {
+                    //Updates the variables
+                    theModel.intRoleData = theModel.StringToIntArray1(theModel.strMessage[4]);
+
+                    //Sends a message in chat
+                    switch (theModel.strMessage[2]) {
+                        case "0" -> theServerLobby.theChatArea.append("Server: " + theModel.strMessage[0] + " has switched to Spectator\n");
+                        case "1" -> theServerLobby.theChatArea.append("Server: " + theModel.strMessage[0] + " has switched to Red\n");
+                        case "2" -> theServerLobby.theChatArea.append("Server: " + theModel.strMessage[0] + " has switched to Black\n");
+                        default -> {}
+                    }
+
+                    //Updates the Panel
+                    theServerLobby.theSpectatorPlayers.setText("Spectators: " + theModel.intRoleData[0]);
+                    theServerLobby.theRedPlayers.setText("Red Players: " + theModel.intRoleData[1]);
+                    theServerLobby.theBlackPlayers.setText("Black Players: " + theModel.intRoleData[2]);
+                }
             }
         }
         theFrame.pack();
+    }
+
+    public void UpdateRoles(String strUsername, String strOldRole, String strNewRole) {
+        //Subtracts from previous role and adds to new role
+        theModel.intRoleData[Integer.parseInt(strOldRole)]--;
+        theModel.intRoleData[Integer.parseInt(strNewRole)]++;
+
+        //Updates the Panel
+        theServerLobby.theSpectatorPlayers.setText("Spectators: " + theModel.intRoleData[0]);
+        theServerLobby.theRedPlayers.setText("Red Players: " + theModel.intRoleData[1]);
+        theServerLobby.theBlackPlayers.setText("Black Players: " + theModel.intRoleData[2]);
+
+        //Updates the Chat
+        updateChat(theModel.strMessage[0], strNewRole);
+
+        //Updates their role
+        theModel.strRole = strNewRole;
+
+        //Sends the message to all clients
+        theModel.sendMessage(theModel.strUsername, "1", theModel.strRole, "2", theModel.ArrayToString1(theModel.intRoleData), null, null);
+    }
+
+    public void SwitchRoles(String strNewRole) {
+        if (theModel.blnIsHost) {
+            UpdateRoles(theModel.strUsername, theModel.strRole, strNewRole);
+        } else {
+            theModel.sendMessage(theModel.strUsername, "0", theModel.strRole, "2", strNewRole, null, null);
+            theModel.strRole = strNewRole;
+            updateChat(theModel.strUsername, strNewRole);
+        }
+    }
+
+    public void updateChat(String strUsername, String strRole) {
+        switch (strRole) {
+            case "0" ->
+                theServerLobby.theChatArea.append("Server: " + strUsername + " has switched to Spectator\n");
+            case "1" ->
+                theServerLobby.theChatArea.append("Server: " + strUsername + " has switched to Red\n");
+            case "2" ->
+                theServerLobby.theChatArea.append("Server: " + strUsername + " has switched to Black\n");
+            default -> {
+            }
+        }
     }
 
     public View() {
