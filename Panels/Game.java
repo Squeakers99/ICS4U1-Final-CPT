f0088703ff8e432b63568d2521e09e297e54bedc
Jayred Robles <jayred.robles@gmail.com>
2024-06-04 10:00:35 -0400

    game logic work

--- Panels/Game.java
+++ Panels/Game.java
@@ -1,5 +1,5 @@
 package Panels;
-
+import java.io.*;
 import java.awt.*;
 import javax.swing.*;
 
@@ -9,8 +9,29 @@ public class Game extends JPanel {
         this.setPreferredSize(new Dimension(1280, 720));
         this.setLayout(null);
     }
-    public static void GameLogic(){
-        int intBoard[][] = new int[8][8];
+    public static void GameLogic() throws IOException{
+        BufferedReader keyboard = new BufferedReader(new InputStreamReader(System.in));
+        String chrBoard[][] = new String[8][8];
+        int intRed = 12;
+        int intBlack = 12;
+        int intTurn = 0;
+        boolean blnTurnRed;
+        FileReader theFileReader = new FileReader("Maps/currentBoard.csv");
+        BufferedReader theBufferedReader = new BufferedReader(theFileReader);
+
+        for(int intLoop = 0; intLoop < 8; intLoop++){
+            chrBoard[intLoop] = theBufferedReader.readLine().split(",");
+        }
+
+        //for(char[] c:chrBoard)
+          //  for(char letter:c)
+
+
+
+        while(intRed > 0 && intBlack > 0){
+            
+        }
+
         
     
     }
